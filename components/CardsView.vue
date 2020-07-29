95e5a0506daa0f3508d5396cf0d6d3838c387453
Kai <ksah466@aisbmali.org>
2020-07-29 11:07:28 +0000

    Updated API and cached projects in localStorage

--- components/CardsView.vue
+++ components/CardsView.vue
@@ -14,69 +14,24 @@
 <script>
 module.exports = {
   mounted() {
-    Promise.all([this.getGithub(), this.getGlitch(), this.getCustom()]).then(
-      values => {
-        let allProjects = values
-          .flat()
-          .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
-        console.log(allProjects);
-        this.projects = allProjects;
-      }
-    );
+    let localProjects = localStorage.getItem("projects");
+    if (localProjects) {
+      this.projects = JSON.parse(localProjects);
+    }
+    fetch("https://api.kaustav.ml/projects.json")
+      .then((data) => data.json())
+      .then(({ data }) => {
+        this.projects = data;
+        localStorage.setItem("projects", JSON.stringify(data));
+      });
   },
   data() {
     return {
-      projects: []
+      projects: [],
     };
   },
-  methods: {
-    getGithub() {
-      return fetch("https://api.github.com/users/ksah466/repos")
-        .then(response => response.json())
-        .then(response =>
-          response
-            .filter(
-              element =>
-                element["has_pages"] &&
-                element["homepage"] != null &&
-                element["name"] != "ksah466.github.io"
-            )
-            .map(({ name, description, homepage, updated_at }) => {
-              return {
-                name,
-                description,
-                homepage,
-                updated_at
-              };
-            })
-        );
-    },
-    getGlitch() {
-      return fetch(
-        "https://api.glitch.com/v1/collections/by/id/projects?id=55957&limit=100"
-      )
-        .then(response => response.json())
-        .then(({ items }) =>
-          items.map(({ domain, description, updatedAt }) => {
-            return {
-              name: domain,
-              description,
-              homepage: `https://${domain}.glitch.me`,
-              updated_at: updatedAt
-            };
-          })
-        );
-    },
-    getCustom() {
-      return fetch(
-        "https://gist.githubusercontent.com/ksah466/b21a03b47585eb87a99181a29e48cd19/raw/projects.json"
-      )
-        .then(data => data.json())
-        .then(({ projects }) => projects);
-    }
-  },
   components: {
-    card: window.httpVueLoader("./components/Card.vue")
-  }
+    card: window.httpVueLoader("./components/Card.vue"),
+  },
 };
 </script>
