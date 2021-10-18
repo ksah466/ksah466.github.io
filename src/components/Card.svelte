676fe33f9b0eb0b2efaf3d001c7c0f5ac583d544
Kai <ksah466@aisbmali.org>
2021-10-18 21:25:18 +0000

    Fix: Updated API and some vulnerabilities

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,20 +1,22 @@
 <script>
-    export let title;
-    export let description;
-    export let image;
-    export let url;
+  export let title;
+  export let description;
+  export let image;
+  export let url;
 </script>
 
 <div class="m-2 max-w-sm rounded overflow-hidden shadow-lg flex flex-col">
-    <img class="w-full" src={image} style="display: none" alt={title} onload="this.style.display=''" />
-    <div class="px-4 py-2">
-      <div class="font-bold text-xl mb-2">{title}</div>
-      <p class="text-gray-700 text-base">{description}</p>
-    </div>
-    <div class="p-4 mt-auto">
-      <a
-        href={url}
-        class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >Visit</a>
-    </div>
+  <img class="w-full" src={image} alt={title} />
+  <div class="px-4 py-2">
+    <div class="font-bold text-xl mb-2">{title}</div>
+    <p class="text-gray-700 text-base">{description}</p>
   </div>
+  <div class="p-4 mt-auto">
+    <a
+      href={url}
+      class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Visit
+    </a>
+  </div>
+</div>
--- src/components/CardsView.svelte
+++ src/components/CardsView.svelte
@@ -9,21 +9,27 @@
     if (localProjects) {
       projects = JSON.parse(localProjects);
     }
-    fetch("https://api.kaustav.ml/projects.json")
+    fetch("https://api.kaustav.ml/projects")
       .then((data) => data.json())
-      .then(({ data }) => {
+      .then((data) => {
         projects = data;
         localStorage.setItem("projects", JSON.stringify(data));
       });
   });
+
+  function getScreenshotLink(website) {
+    const urlWIthoutProtocol = website.split("://")[1];
+    return `https://cdn.statically.io/screenshot/${urlWIthoutProtocol}`;
+  }
 </script>
 
 <div class="grid grid-cols-1 md:grid-cols-3">
   {#each projects as project}
     <Card
-      image={`https://image.thum.io/get/${project.homepage}`}
-      url={project.homepage}
+      image={getScreenshotLink(project.url)}
+      url={project.url}
       title={project.name}
-      description={project.description} />
+      description={project.description}
+    />
   {/each}
 </div>
